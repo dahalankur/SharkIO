24ad6ac34bf9c5797b055ce82a337b5b91ac3ec8
rujenamatya <rru.jju@gmail.com>
2022-03-31 19:27:29 -0400

    added player object

---
+++ src/player.py
@@ -0,0 +1,28 @@
+"""
+gameobject.py
+Implements a Player class
+"""
+
+class Player():
+    """
+    An instance of this class represents a player object which contains
+    a list of chunks and it's camera view
+    """
+    def __init__(self, chunks_list, camera_view):
+      """
+      Initialize the Player object with the list of chunk and camera view
+      """
+      self.__chunks_list = chunks_list
+      self.__camera_view = camera_view
+
+    def add_chunk(self, chunk_id):
+      """
+      Adds the chunk with the given chunk_id to the chunks_list
+      """
+      self.__chunks_list.append(chunk_id)
+
+    def remove_chunk(self, chunk_id):
+      """
+      Removes the chunk with the given chunk_id to the chunks_list
+      """
+      self.__chunks_list.remove(chunk_id)
