20b9a8e62bd978c520e1acbfa05f2d8e8574367e
Ankur Dahal <dahalankur123@gmail.com>
2022-03-31 19:59:38 -0400

    Add test framework for chunk

--- src/chunk.py
+++ src/chunk.py
@@ -43,3 +43,43 @@ class Chunk(GameObject):
               An id for a chunk is of the form parentID:chunkID
         """
         return str(self.__id) + ":" + str(self.__sub_id)
+
+
+# def tests():
+#     # player1 = GameObject(100, 100, 3, (200, 0, 0), 'chunk', \
+#     #                     random.randint(1 , 2))
+#     # player2 = GameObject(200, 100, 5, (200, 0, 222), 'chunk', \
+#     #                     random.randint(1 , 200))
+#     # player3 = Chunk(100, 100, )
+    
+#     assert(player1.is_colliding(virus))
+#     assert(not player1.is_colliding(player2))
+#     assert(not player1.is_colliding(food1))
+#     assert(not player1.is_colliding(food2))
+#     assert(not food1.is_colliding(food2))
+
+#     assert(virus.get_color() == (0, 255, 0))
+#     assert(player1.get_color() == (200, 0, 0))
+
+#     assert(virus.get_type() == 'virus')
+#     assert(player1.get_type() == 'chunk')
+#     assert(player2.get_type() == 'chunk')
+#     assert(food1.get_type() == 'food')
+#     assert(food2.get_type() == 'food')
+
+#     assert(virus.is_virus())
+#     assert(player1.is_chunk())
+#     assert(player2.is_chunk())
+#     assert(food1.is_food())
+#     assert(food2.is_food())
+
+#     assert(food1.get_radius() == 5)
+#     assert(food2.get_radius() == 1)
+#     assert(virus.get_radius() == 4)
+
+#     assert(player1.get_pos() == (100, 100))
+#     assert(player2.get_pos() == (200, 100))
+#     assert(virus.get_pos() == (100, 102.345))
+
+# if __name__ == '__main__':
+#     tests()
