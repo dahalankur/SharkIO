f5f01be58e87dceebf81d5dd300e20658710d3d9
Ankur Dahal <dahalankur123@gmail.com>
2022-04-03 15:26:35 -0400

    Add file description to camera.py and add additional comments

--- src/camera.py
+++ src/camera.py
@@ -1,3 +1,8 @@
+"""
+camera.py
+Implements a Camera class 
+Authors: Ankur Dahal, Ellis Brown, Jackson Parsells, Rujen Amatya
+"""
 # class.
 # parameter will be a playyer, and this will be held as a reference
 # The camera will take the average weighted posistion of all the 
@@ -9,3 +14,11 @@
 # the camera be bounded by the corners, and not move off the boarder.
 # The camera will be updated every time the server sends the client
 # an update.
+
+# Edits from Ankur:
+# The camera will not be "updated" per se. The client will only be rendering 
+# what the camera sees in its rectangle and it will do the calculation -> 
+# the goal of the camera is to pick a center of mass of a player's chunks that 
+# it is attached to and the client will look at this information, along with 
+# the camera's bounds (width/height) and will render the relevant part of the 
+# canvas on the client's screen. That's all, as far as I understand so far.
