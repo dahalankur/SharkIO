b32fc363a322d7e4b42624d8f533f2f9011a4973
Ankur Dahal <dahalankur123@gmail.com>
2022-04-06 16:55:32 -0400

    Fix style

--- src/gameobject.py
+++ src/gameobject.py
@@ -122,6 +122,7 @@ class GameObject():
 
 def tests():
     import random
+    
     virus = GameObject(100, 102.345, 4, (0, 255, 0), 'virus', \
                        random.randint(1, 10))
     player1 = GameObject(100, 100, 3, (200, 0, 0), 'chunk', \
