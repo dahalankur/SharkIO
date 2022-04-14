ca113214255b4c9bd8fcce9d4f74c0114649df66
Ankur Dahal <dahalankur123@gmail.com>
2022-04-14 10:12:47 -0400

    Modify client to work with updated player

--- src/client.py
+++ src/client.py
@@ -14,16 +14,21 @@ FPS = 30
 WIDTH = HEIGHT = 600
 WHITE = pygame.Color(255, 255, 255)
 
-def run_client():
-    """
-    Initializes pygame and renders required components for the client, 
-    while also communicating game state changes to the server via sockets
-    """
+
+def init_pygame():
     pygame.init()
     screen = pygame.display.set_mode([WIDTH, HEIGHT])
     clock = pygame.time.Clock()
     font = pygame.font.SysFont("segoe ui", 34, True)
     font2 = pygame.font.SysFont("segoe ui", 15, True)
+    return screen, clock, font, font2
+
+
+def run_client():
+    """
+    Initializes pygame and renders required components for the client, 
+    while also communicating game state changes to the server via sockets
+    """
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.connect((HOST, PORT))
@@ -32,6 +37,7 @@ def run_client():
         name = bytes(input("Enter your name: ").encode('utf-8'))
         send_data(sock, name)
 
+        screen, clock, font, font2 = init_pygame()
         running = True
 
         while running:
@@ -43,39 +49,42 @@ def run_client():
             
             clock.tick(FPS)
             screen.fill(WHITE)
-            chunks = player.get_chunks()
+            chunk = player.get_chunk()
             mousex, mousey = pygame.mouse.get_pos()
-            for _, chunk in chunks.items():
-                (posx, posy) = chunk.get_pos()
-                radius = chunk.get_radius()
-                velocity = chunk.get_velocity()
-                color = chunk.get_color()
-                if posx < mousex - (radius * 1.25): # TODO: adjust the constant accordingly
-                    # move right
-                    posx = min(WIDTH, posx + velocity)
-                elif posx > mousex + (radius * 1.25):
-                    # move left
-                    posx = max(0, posx - velocity)
-                if posy > mousey + (radius * 1.25):
-                    # move down
-                    posy = max(0, posy - velocity)
-                elif posy < mousey - (radius * 1.25):
-                    # move up
-                    posy = min(HEIGHT, posy + velocity)
-                chunk.set_pos(posx, posy) # TODO: might have to change once camera and perspective comes into play
             
-            ## send the chunks data to the server.
-            data = pickle.dumps(chunks)
-            send_data(sock, data)
+            (posx, posy) = chunk.get_pos()
+            radius = chunk.get_radius()
+            velocity = chunk.get_velocity()
+            color = chunk.get_color()
+            if posx < mousex - (radius * 1.25): # TODO: adjust the constant accordingly
+                # move right
+                posx = min(WIDTH, posx + velocity)
+            elif posx > mousex + (radius * 1.25):
+                # move left
+                posx = max(0, posx - velocity)
+            if posy > mousey + (radius * 1.25):
+                # move down
+                posy = max(0, posy - velocity)
+            elif posy < mousey - (radius * 1.25):
+                # move up
+                posy = min(HEIGHT, posy + velocity)
+            
+            chunk.set_pos(posx, posy) # TODO: might have to change once camera and perspective comes into play
+            
+
 
             # Draw the game objects
             for _, object in objects_dict.items():
                 # a check for seeing if that object is in the player's view or not goes here. maybe we do not need an explicit camera, we could calculate the render rectangle dynamically every time
                 pygame.draw.circle(screen, object.get_color(), object.get_pos(), object.get_radius())
             
-            # render score UI
-            score = player.get_score()
-            text = font.render("Score: " + str(int(score)), True, (255, 0, 0))
+            # Draw the players
+            for p in players.values():
+                p_chunk = p.get_chunk()
+                pygame.draw.circle(screen, p_chunk.get_color(), p_chunk.get_pos(), p_chunk.get_radius())
+            
+             # render score UI
+            text = font.render("Score: " + str(int(player.get_score())), True, (255, 0, 0))
             screen.blit(text, (30, 550))
 
             # render leaderboard
@@ -93,13 +102,17 @@ def run_client():
               text3 = font2.render(leader_txt2, True, (255, 0, 0))
               screen.blit(text3, (480, loc))
 
-
+            # send the updated chunk data to the server.
+            data = pickle.dumps(chunk)
+            send_data(sock, data)
+            
             pygame.display.flip()
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     # TODO: send message to the server that I am disconnecting
                     running = False
+    
     pygame.quit()
 
 
