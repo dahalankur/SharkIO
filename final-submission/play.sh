cd6206621d41eeaaea79c963c1dbabda9051d095
Ankur Dahal <dahalankur123@gmail.com>
2022-04-29 18:39:32 -0400

    Remove final submission directory

--- final-submission/play.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# setup.sh
-# A shell script to run the game client
-
-set -euo pipefail
-
-echo "[*] Running the game client"
-cd src && ../venv/bin/python3 client.py
--- final-submission/server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# setup.sh
-# A shell script to run the server
-
-set -euo pipefail
-
-echo "[*] Running the game server"
-cd src && ../venv/bin/python3 server.py
